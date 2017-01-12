23fdde06be75281a5590e3cbc12d46e7a9205c77
Anthony FAVIER <anthony.favier03@gmail.com>
2017-01-12 22:18:49 +0100

    majMobilFirst

---
+++ script/cv.js
@@ -0,0 +1,18 @@
+///////////////////////////////MENU--DEROULANT///////////////////////////////////////////////////////////////
+function menuHamburger(){
+	document.getElementById("dropDown").classList.toggle("show");
+}
+window.onclick = function(event){
+	if (!event.target.matches('.hamburger')) {
+		var dropdowns = document.getElementByClassName("menu");
+		var i;
+		for (i=0; i<dropdowns.length; i++){
+			var openDropdown = dropdowns[i];
+			if (openDropdown.classList.contains("show")){
+				openDropdown.classList.remove("show");
+			}
+		}
+	}
+}
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////-
